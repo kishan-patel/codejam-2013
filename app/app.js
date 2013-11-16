b14b01ee90a82e1c14eba05157ef2c0080751499
Rathesh Saba <rathesh.saba@gmail.com>
2013-11-15 23:35:30 -0500

    Data is recieved and partially displayed

--- app/app.js
+++ app/app.js
@@ -8,6 +8,7 @@ var io = require('socket.io').listen(httpServer);
 
 //App configurations
 app.configure(function(){
+  app.use(express.bodyParser());
   app.use(app.router);
   app.use(express.static(__dirname+'/public'));
   app.use(express.static(__dirname+'/lib'));
@@ -15,6 +16,19 @@ app.configure(function(){
 
 app.get('/', function(req, res){
   res.sendfile('public/index.html');
+});
+app.get('/csv',function(req,res){
+  res.sendfile('public/csv.html');
+});
+app.post('/machine',function(req,res) {
+  var x = req.files.file.path;
+  var csv = require('ya-csv');
+  var reader = csv.createCsvFileReader(x,{'columnsFromHeader':true, 'separator':','});
+  reader.addListener('data',function(data){
+    server.csvClientData(data);
+    console.log(data);
+  });
+  res.send("hello");
 });
 
 server.start(io);
